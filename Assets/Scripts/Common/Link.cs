64a861d8c5f2b51710aef516e21abf9d057643e7
Syo Igarashi <igara1119@gmail.com>
2019-06-22 13:07:25 +0900

    modify: move to link

---
+++ Assets/Scripts/Common/Link.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Link
+{
+    public static void Move(string url)
+    {
+#if UNITY_EDITOR
+        Application.OpenURL(url);
+#elif UNITY_WEBGL
+        Application.ExternalEval(string.Format("window.open('{0}','_blank')", url));
+#else
+        Application.OpenURL(url);
+#endif
+    }
+}
---
+++ Assets/Scripts/Scenes/CreditScene/CreditScrollViewScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditScrollViewScript : MonoBehaviour
+{
+
+    public void PointerDownUnity()
+    {
+        Link.Move("https://unity.com");
+    }
+
+    public void PointerDownNotoSans()
+    {
+        Link.Move("https://www.google.com/get/noto/#sans-jpan");
+    }
+
+    public void PointerDownUnityChan()
+    {
+        Link.Move("http://unity-chan.com");
+    }
+
+    public void PointerDownHQFightingAnimationFREEText()
+    {
+        Link.Move("https://assetstore.unity.com/packages/3d/animations/hq-fighting-animation-free-33478");
+    }
+
+    public void PointerDownFighterPackBundleFREEText()
+    {
+        Link.Move("https://assetstore.unity.com/packages/3d/animations/fighter-pack-bundle-free-36286");
+    }
+
+    public void PointerDownRealStarsSkyboxText()
+    {
+        Link.Move("https://assetstore.unity.com/packages/3d/environments/sci-fi/real-stars-skybox-116333");
+    }
+
+    public void PointerDownSpaceRobotKyleText()
+    {
+        Link.Move("https://assetstore.unity.com/packages/3d/characters/robots/space-robot-kyle-4696");
+    }
+
+    public void PointerDownUnityParticlePackText()
+    {
+        Link.Move("https://assetstore.unity.com/packages/essentials/tutorial-projects/unity-particle-pack-127325");
+    }
+
+    public void PointerDownUnityWebSocketText()
+    {
+        Link.Move("https://github.com/y85171642/UnityWebSocket");
+    }
+}
