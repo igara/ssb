6016e277a15c9ca41edf2cbaba9040e4e214730a
Syo Igarashi <igara1119@gmail.com>
2019-08-17 14:12:24 +0900

    change: wss protocol.

--- Assets/Scripts/Common/GameSetting.cs
+++ Assets/Scripts/Common/GameSetting.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class GameSetting
 {
-    // public static string webSoketServerUri = "ws://localhost:9000";
-    public static string webSoketServerUri = "wss://syonet.work/ssb/";
+    public static string webSoketServerUri = "wss://localhost/ssb/";
+    // public static string webSoketServerUri = "wss://syonet.work/ssb/";
     public static UserData selfUserData = new UserData();
     public static UserData[] cpuUserDatas = new UserData[1] {
         new UserData()
